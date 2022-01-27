209b8af2eda69b4368cbf34edba483c3b3ee4c96
Andressa Kotsugai <andressakotsugai@gmail.com>
2022-01-26 22:08:13 -0300

    create service

--- src/services/todoService.js
+++ src/services/todoService.js
@@ -1,1 +1,44 @@
-const BaseUrl = "localhost:8000/todos";
+const BaseUrl = `${process.env.REACT_APP_API_URL}/todos`;
+
+export const loadTodos = () => {
+  return fetch(baseUrl).then((res) => res.json());
+};
+
+export const getTodo = (id) => {
+  return fetch(`${baseUrl}/${id}`).then((res) => res.json());
+};
+
+export const createTodo = (todo) => {
+  return fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+
+    body: JSON.stringify({
+      title: todo.title,
+      completed: todo.completed,
+    }),
+  }).then((res) => res.json());
+};
+
+export const updateTodo = (todo) => {
+  return fetch(`${baseUrl}/${todo.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json",
+    },
+
+    body: JSON.stringify({
+      id: todo.id,
+      title: todo.title,
+      completed: todo.completed,
+    }),
+  }).then((res) => res.json());
+};
+
+export const deleteTodo = (id) => {
+    return fetch(`${baseUrl}/${id}`,{
+        method: "DELETE"
+    }).then(res => res.json())
+}
